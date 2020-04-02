484bcb3cf35db849ce19bac4a064a1744eedabad
stillshotoffice <resavestation@gmail.com>
2020-04-03 01:35:11 +0800

    測試小工具網站

---
+++ precache-manifest.a598fe114adafcb81db4e1fec352a366.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3373aed6e7f8494d636c80761c768e3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a06b882432fbbd912648",
+    "url": "/static/css/2.52de5766.chunk.css"
+  },
+  {
+    "revision": "014c1ec4cf075d928c87",
+    "url": "/static/css/main.0715a621.chunk.css"
+  },
+  {
+    "revision": "a06b882432fbbd912648",
+    "url": "/static/js/2.224fa46e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.224fa46e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014c1ec4cf075d928c87",
+    "url": "/static/js/main.a112a8e9.chunk.js"
+  },
+  {
+    "revision": "9e1bac42f9b434e39430",
+    "url": "/static/js/runtime-main.74e01f80.js"
+  }
+]);
